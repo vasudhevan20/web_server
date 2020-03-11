07aa570743d043297d60238f7d6690aa87de51a4
vasudhevan20 <62016861+vasudhevan20@users.noreply.github.com>
2020-03-11 11:03:31 +0530

    Update uwsc.c

--- tools/uwsc/uwsc.c
+++ tools/uwsc/uwsc.c
@@ -25,7 +25,7 @@
 #include <stdlib.h>
 #include <getopt.h>
 #include <ulfius.h>
-#include "orcania.h"
+#include <orcania.h>
 
 /** Define mock yder functions when yder is disabled **/
 #ifdef U_DISABLE_YDER
